61f7ee7daf95285acdc3b16c8f85c2bafe5133a0
Loubnaelghazi <loubna.elghazi1@etu.uae.ac.ma>
2023-11-04 22:46:44 +0100

    ..

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="App">
+      <p className="text-primary_color  text-3xl">hello</p>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary_color: "#3490dc", // Its just a test 
+      },
+    },
   },
   plugins: [],
 };
